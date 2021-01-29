9a363ce9e75c644ae135fa89402a63eea966a24b
starter <Starternh@gmail.com>
2021-01-29 10:45:54 +0300

    fixes

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 echo "Clone posts API repository"
-#git clone -b develop ssh://.git api
+git clone -b master git@github.com:Starternt/posts-api.git
+
+echo "Clone votes API repository"
+git clone -b master git@github.com:Starternt/votes-api.git
 
 sleep 2; curl -u "admin:admin" -H 'Accept: application/json' -H 'Content-Type: application/json' -H 'X-Requested-By: curl' -d @"etc/graylog/input-gelf-udp.json" "http://127.0.0.1:9090/api/system/inputs?pretty=true"
--- scripts/gateway/conf.hcl
+++ scripts/gateway/conf.hcl
@@ -3,6 +3,11 @@ template {
   destination = "/etc/nginx/conf.d/default.conf"
 }
 
+template {
+  source = "/etc/nginx/conf.d/votes.conf.tpl"
+  destination = "/etc/nginx/conf.d/votes.conf"
+}
+
 exec {
   command = "nginx -g \"daemon off;\""
 }
